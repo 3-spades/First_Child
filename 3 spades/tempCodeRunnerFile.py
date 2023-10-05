a1f0b74c05942306c446bd8f44526308cc4e3f56
Hoang Tien Dat <117803020+GoddessH@users.noreply.github.com>
2023-10-05 11:52:01 +0700

    Add files via upload

---
+++ 3 spades/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+d = {
+    "Code": [
+        {
+            "Answer": 13,
+            "Question": 523
+        }
+    ],
+
+    "Eat": [
+        {
+            "Food": "Banana",
+            "Drink": "Cola"
+        }
+    ]
+}
+
+for i in d:
+    print(i)
